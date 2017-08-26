8fd60f7f4965d495605d109373a49d3a7675cc64
Sean Williams <seanasdf@gmail.com>
2017-08-26 18:07:25 -0500

    finished laps table, starting vacation stops table

--- p24.R
+++ p24.R
@@ -25,23 +25,33 @@ if (file.exists("teamdata.rda")) {
   tm_name <- map(teampages, html_nodes, "h3") %>% 
     map(html_text) 
   
+  #get team category from leaderboard
+  tm_cat <- map(teampages, html_nodes, "dd:nth-child(2)") %>% 
+    map_chr(html_text)
+  
   #get team data from team page
   tm_data <- map(teampages, html_nodes, "#laps-list-table") %>% 
     map(html_table)
   
+  
   #save objects to cache them
-  save(tm_name, tm_data, file="teamdata.rda")
+  save(ldrboard, tm_cat, tm_name, tm_data, file="teamdata.rda")
 }
 
-# cumulative_laps <- function(df) {
-#   hours <- mdy_hm("08-18-2017, 19:00") + hours(1)*c(1:23)
-#   df <- map(hours, function(x) {
-#     mutate(df, ifelse(datetime<x), )
-#     })
-# }
+#unlist and clean up team names
+tm_name <- unlist(tm_name) %>% 
+  map_chr(trimws, which="both")
 
-laps <- map(tm_data, 1) %>% 
-  map(mutate, rider_num = str_extract(Rider, "\\([0-9]{1,3}"),
+
+#Pull all laps into one big Df, calculate datetime of each lap.
+laps <- map(tm_data, 1) %>%
+  map2(tm_name,
+          ~mutate(.x, team=.y)) %>% 
+  map2(tm_cat,
+       ~mutate(.x, category=.y)) %>% 
+  bind_rows() %>% 
+  tbl_df() %>% 
+  mutate(rider_num = str_extract(Rider, "\\([0-9]{1,3}"),
       rider_num = str_extract(rider_num, "[0-9]{1,3}"), 
       rider_name = str_extract(Rider, "^(.*?)\\("),
       rider_name = substr(rider_name, 1, nchar(rider_name)-2),
@@ -49,20 +59,27 @@ laps <- map(tm_data, 1) %>%
       day = ifelse(hour(time)>=19 & hour(time) <=23, "Friday", "Saturday"),
       datetime = ifelse(day=="Friday", paste0("8/18/2017, ", Time), paste0("8/19/2017, ", Time)),
       datetime = mdy_hms(datetime)
-  ) 
+  ) %>% 
+  select(-Time, -Rider, -time)
 
+
+# get breakpoints for hours and variable
 hourbreaks <- mdy_hm("08-18-2017, 19:00") + hours(1)*c(1:23) 
 vars <- map_chr(hourbreaks, function(x) paste0("hour", 
                                                ifelse(hour(x)>=20, hour(x)-19, hour(x)+5)))
 
-test <- laps %>% 
-  map(function(df) map2_df(.x=vars, 
-                           .y=hourbreaks,
-                           .f = function(.x,.y) {
-                             varname <- quo_name(.x)
-                             time <- .y
-                             #print(df$datetime < time)
-                             mutate(df, !!varname := sum(datetime<time))
-                           })
-  )
+# number of laps at each hour interval, by team
+laps <- map2_df(vars, hourbreaks, function(x,y) {
+  varname <- quo_name(x)
+  laps <- laps %>% 
+    group_by(team) %>% 
+    mutate(!!varname := sum(datetime<y))
+})
 
+
+#vacation stops
+vacay <- tbl_df(tm_name) %>% 
+  set_names("team") 
+
+blah <- map(tm_data, 2)
+blah <- replace(blah, map(blah, is.null), "Z")
